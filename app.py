851458031b007bf5d88537c5153180ac21697d1f
sophercooper <sophercooper@outlook.com>
2025-10-06 08:59:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'dan.remus56@gmail.com'  # Your Gmail email address
-app.config['MAIL_PASSWORD'] = 'galzpmulxdfisphu'  # Your Gmail password or App Password
+app.config['MAIL_PASSWORD'] = 'dskyyhmtkbbetgqs'  # Your Gmail password or App Password
 
 mail = Mail(app)
 
